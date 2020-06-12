da954179e89d654f20766847de53b86b9018d9b1
Luke Lau <luke@whiletrue.codes>
2020-06-12 16:57:43 +0800

    Enhance scan rate

--- lib/src/qr_painter.dart
+++ lib/src/qr_painter.dart
@@ -257,7 +257,7 @@ class QrPainter extends CustomPainter {
   bool _isLogoArea(int x, int y) {
     //Find center of module count and portion to cut out of QR
     var center = _qr.moduleCount / 2;
-    var canvasPortion = _qr.moduleCount * 0.15;
+    var canvasPortion = _qr.moduleCount * 0.13;
 
     if (x > center - canvasPortion &&
         x < center + canvasPortion &&
